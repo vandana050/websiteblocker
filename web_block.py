98a7a18418d0aae5f857af3d40552908f3d88275
vandana050 <157218933+vandana050@users.noreply.github.com>
2025-10-31 10:23:25 +0530

    Update web_block.py

--- web_block.py
+++ web_block.py
@@ -1,34 +1,32 @@
 import time
 from datetime import datetime as dt
 
-ip_localmachine="127.0.0.1"
-website_list=["instagram.com","spankbang.com"]
-hosts_path="C:\Windows\System32\drivers\etc\hosts"
-start_time="09:0:0"
-end_time="15:0:0"
-    
-now=dt.now()
-current_time=now.strftime("%H:%M:%S")
-print(current_time)
+ip_localmachine = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com", "www.instagram.com", "instagram.com"]
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+
+start_time = dt.strptime("09:00:00", "%H:%M:%S").time()
+end_time = dt.strptime("14:00:00", "%H:%M:%S").time()
 
 while True:
-    current_time=now.strftime("%H:%M:%S")
-    if start_time<=current_time and current_time<=end_time:
-        print("Working hours")
-        with open(hosts_path,"r+") as file:
-            content=file.read()
+    now = dt.now()
+    current_time = now.time()
+
+    if start_time <= current_time <= end_time:
+        print("Working hours: blocking sites")
+        with open(hosts_path, "r+") as file:
+            content = file.read()
             for website in website_list:
-                if website in content:
-                    pass
-                
-                else:
-                    file.write(ip_localmachine+" "+website+"\n")
-            
+                if website not in content:
+                    file.write(ip_localmachine + " " + website + "\n")
     else:
-        print("Non working hours")
-        with open(hosts_path,"r+") as file:
-            file.truncate(0)
-            file.close()
-            
-    
+        print("Non-working hours: unblocking sites")
+        with open(hosts_path, "r+") as file:
+            lines = file.readlines()
+            file.seek(0)
+            for line in lines:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+
     time.sleep(10)
